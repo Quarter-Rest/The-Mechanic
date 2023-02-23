e932424687ba94a4ce84a037f50db7d51a53f24e
GTHanson <57633842+GTHanson@users.noreply.github.com>
2023-02-23 00:13:24 -0800

    s

---
+++ commands/misc/recipe.js
@@ -0,0 +1,34 @@
+const { OPENAI_SECRET_KEY } = require("../config.json");
+const { Configuration, OpenAIApi } = require("openai");
+const configuration = new Configuration({
+  apiKey: OPENAI_SECRET_KEY,
+});
+const openai = new OpenAIApi(configuration);
+
+module.exports = {
+	name: "recipe",
+
+	/** You need to uncomment below properties if you need them. */
+	//description: 'Ping!',
+	//usage: 'put usage here',
+	//permissions: 'SEND_MESSAGES',
+	//guildOnly: true,
+
+	execute(message, args) {
+        message.react('770876050318032896');
+        print(message.content);
+		var prompt = currentPers + `List a recipe on how to make  \'${message.content}\'`;
+        (async () => {
+            const gptResponse = await openai.createCompletion({
+                model: "text-davinci-003",
+                prompt: prompt,
+                temperature: 0.6,
+                max_tokens: 512,
+              });
+
+
+            message.reply(`${gptResponse.data.choices[0].text}`);
+
+        })();
+	},
+};
