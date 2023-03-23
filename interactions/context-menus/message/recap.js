b06dc4db8bfbcb04e19d07ccb1c3aec502279383
GTHanson <57633842+GTHanson@users.noreply.github.com>
2023-03-22 21:57:28 -0700

    Update recap.js

--- interactions/context-menus/message/recap.js
+++ interactions/context-menus/message/recap.js
@@ -30,7 +30,7 @@ module.exports = {
         const openai = new OpenAIApi(configuration);
         (async () => {
             const gptResponse = await openai.createCompletion({
-                model: "gpt-3.5-turbo",
+                model: "text-davinci-003",
                 prompt: prompt,
                 temperature: 0.6,
                 max_tokens: 4096,
