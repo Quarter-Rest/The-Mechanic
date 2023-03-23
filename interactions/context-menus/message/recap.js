13a5aea9f20e503ebc440bc021ef508bf94a1e8b
GTHanson <57633842+GTHanson@users.noreply.github.com>
2023-03-22 21:47:25 -0700

    Update recap.js

--- interactions/context-menus/message/recap.js
+++ interactions/context-menus/message/recap.js
@@ -35,7 +35,7 @@ module.exports = {
                 temperature: 0.6,
                 max_tokens: 4096,
               });
-              await interaction.reply(`${gptResponse.data.choices[0].text}`);
+              await message.channel.send(`${gptResponse.data.choices[0].text}`);
 
         })();
 		return;
