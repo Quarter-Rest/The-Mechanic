3c4df42c1da2a1857ff77e8b3378f8e273f8620f
GTHanson <57633842+GTHanson@users.noreply.github.com>
2023-03-22 21:40:32 -0700

    Update recap.js

--- interactions/context-menus/message/recap.js
+++ interactions/context-menus/message/recap.js
@@ -6,6 +6,14 @@ const {
     v1: uuidv1,
     v4: uuidv4,
   } = require('uuid');
+
+const { OPENAI_SECRET_KEY } = require("../config.json");
+const { Configuration, OpenAIApi } = require("openai");
+const configuration = new Configuration({
+apiKey: OPENAI_SECRET_KEY,
+});
+const openai = new OpenAIApi(configuration);
+
 module.exports = {
 	data: {
 		name: "Recap",
@@ -19,42 +27,18 @@ module.exports = {
 	async execute(interaction) {
         var message = await interaction.channel.messages.fetch(interaction.targetId);
         message.react('770876050318032896');
-        const requestId = uuidv4();
-        const api = generate({
-            prompt: message.content,
-            apiKey: DREAMSTUDIO_API_KEY,
-            requestId,
-            host: 'https://grpc.stability.ai:443',
-            engine: 'stable-diffusion-v1',
-            width: 512,
-            height: 512,
-            diffusion:'k_lms',
-            outDir: process.cwd() + '/images/' + uuidv4(),
-            steps: 20,
-            cfgScale: 7,
-            samples: 1
-        })
-        var filePath = "";
-        api.on('image', ({ buffer, filePath }) => {
-            filePath = filePath.substring(filePath.search("images"));
-            
-            const embed = new MessageEmbed()
-            .setTitle(`Generated Image by ${interaction.member.nickname}`)
-            .setDescription(`\"${message.content}\"`);
-            
-            message.channel.send({embeds:[embed]});
-            message.channel.send({files: [
-                { 
-                  attachment: `./${filePath}`,
-                  name: `${requestId}.png` 
-                }
-            ]});
-        })
-          
-        api.on('end', (data) => {
-        })
         
-		await interaction.reply({ content: 'Working on it!', ephemeral: true });
+        var prompt = `Recap the following messages.\n\"${message.content}\"`;
+        (async () => {
+            const gptResponse = await openai.createCompletion({
+                model: "gpt-3.5-turbo",
+                prompt: prompt,
+                temperature: 0.6,
+                max_tokens: 4096,
+              });
+              await interaction.reply(`${gptResponse.data.choices[0].text}`);
+
+        })();
 		return;
 	},
 };
