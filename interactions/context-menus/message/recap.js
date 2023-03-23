6b73182d1884bd9fe195f81a62c68e9d0e7663e2
GTHanson <57633842+GTHanson@users.noreply.github.com>
2023-03-22 22:41:10 -0700

    Update recap.js

--- interactions/context-menus/message/recap.js
+++ interactions/context-menus/message/recap.js
@@ -33,8 +33,8 @@ module.exports = {
         var lastMessage = message;
 
         var prompt = `Create a recap for the following messages:\n`;
-        const messages = await message.channel.messages.fetch({ limit: 10, cache: false, before: lastMessage.id });
-        messages.reverse()
+        const messages = await message.channel.messages.fetch({ limit: 100, cache: false, before: lastMessage.id });
+        messages.reverse();
         for (let i = 0; i < messages.size; i++) {
             const previousMessage = messages.at(i);
             prompt += `${previousMessage.author.username}:\"${previousMessage.content}\"\n`;
