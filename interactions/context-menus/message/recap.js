05fb7a1c112bd6f61fc38b379154078cdf0dc56a
GTHanson <57633842+GTHanson@users.noreply.github.com>
2023-03-22 22:06:42 -0700

    Update recap.js

--- interactions/context-menus/message/recap.js
+++ interactions/context-menus/message/recap.js
@@ -25,20 +25,27 @@ module.exports = {
 	async execute(interaction) {
         var message = await interaction.channel.messages.fetch(interaction.targetId);
         message.react('770876050318032896');
-        console.log(`"${message.content}\"`);
-        var prompt = `Recap the following messages.\n\"${message.content}\"`;
-        const openai = new OpenAIApi(configuration);
-        (async () => {
-            const gptResponse = await openai.createCompletion({
-                model: "text-davinci-003",
-                prompt: prompt,
-                temperature: 0.6,
-                max_tokens: 1024,
-              });
-              console.log(gptResponse.data.choices[0].text);
-              await message.channel.send(`${gptResponse.data.choices[0].text}`);
+        
+        const openai = new OpenAIApi(
+            new Configuration({ apiKey: process.env.OPENAI_KEY })
+        );
 
-        })();
+        // Setting values for the prompt and message to be used in the GPT-3 and GPT-3.5-Turbo
+        const GPT35TurboMessage = [
+        { role: "system", content: `Create a recap for the following messages:\n${message.content}` },
+        ];
+
+        let GPT35Turbo = async (messagePrompt) => {
+            const response = await openai.createChatCompletion({
+              model: "gpt-3.5-turbo",
+              messages: messagePrompt,
+            });
+          
+            return response.data.choices[0].message.content;
+          };
+
+        console.log("### I'm GPT-3.5-TURBO. ####", await GPT35Turbo(GPT35TurboMessage));
+
 		return;
 	},
 };
