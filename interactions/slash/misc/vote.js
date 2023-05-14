1723f219b290504e5f889b1cc75d4bd0122d29ef
GriffKoke <gtkhanson@gmail.com>
2023-05-13 19:18:28 -0500

    no message

--- interactions/slash/misc/vote.js
+++ interactions/slash/misc/vote.js
@@ -27,7 +27,7 @@ module.exports = {
 		const user = interaction.user;
 		let curTime = Date.now();
 
-		await global.con.query('SELECT * FROM `vote_creation`', function(err, results, fields) {
+		global.con.query('SELECT * FROM `vote_creation`', function(err, results, fields) {
 			if(err)
 			{
 				console.log("SQL Failed")
@@ -65,28 +65,33 @@ module.exports = {
 				interaction.reply({content: `You are on vote cooldown for another ${diff} seconds.`});
 				return;
 			}
+
+			CreateVote(interaction, user, title, desc);
 		});
+	}
 
-		// finished doing checks
-		const exampleEmbed = new MessageEmbed()
-		.setColor(0x0099FF)
-		.setTitle(title)
-		.setAuthor({ name: interaction.user.username, iconURL: interaction.user.avatarURL() })
-		.setDescription(desc)
-		.setThumbnail('https://th.bing.com/th/id/R.7e18af4777dbfce8a8f36e742ac7c318?rik=id88H6t%2fXM9OHA&riu=http%3a%2f%2fwww.technologybloggers.org%2fwp-content%2fuploads%2f2011%2f06%2fThe-United-Nations-logo.png&ehk=LBNkq62sxRCgriL6bRuJ0ZCqP5CPBfRT1mgPSqx3zaI%3d&risl=&pid=ImgRaw&r=0')
-		.setTimestamp()
-
-		const replied = await interaction.reply({ embeds: [exampleEmbed], fetchReply: true });
-		await replied.react('👍');
-		await replied.react('👎');
-		await replied.react('⚪');
-
-		await global.con.query(`UPDATE vote_creation SET last_vote = ${curTime} WHERE id = ${user.id}`, (err, row) => {
-            if (err) {
-                message.channel.send("SQL Failed");
-                return console.error(err);
-            }
-        });
-
-	},
 };
+
+async function CreateVote(interaction, user, title, desc)
+{
+	// finished doing checks
+	const exampleEmbed = new MessageEmbed()
+	.setColor(0x0099FF)
+	.setTitle(title)
+	.setAuthor({ name: interaction.user.username, iconURL: interaction.user.avatarURL() })
+	.setDescription(desc)
+	.setThumbnail('https://th.bing.com/th/id/R.7e18af4777dbfce8a8f36e742ac7c318?rik=id88H6t%2fXM9OHA&riu=http%3a%2f%2fwww.technologybloggers.org%2fwp-content%2fuploads%2f2011%2f06%2fThe-United-Nations-logo.png&ehk=LBNkq62sxRCgriL6bRuJ0ZCqP5CPBfRT1mgPSqx3zaI%3d&risl=&pid=ImgRaw&r=0')
+	.setTimestamp()
+
+	const replied = await interaction.reply({ embeds: [exampleEmbed], fetchReply: true });
+	await replied.react('👍');
+	await replied.react('👎');
+	await replied.react('⚪');
+
+	global.con.query(`UPDATE vote_creation SET last_vote = ${curTime} WHERE id = ${user.id}`, (err, row) => {
+		if (err) {
+			console.log("SQL Failed");
+			return console.error(err);
+		}
+	});
+}
