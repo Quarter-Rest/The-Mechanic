424c66d2ec498e4897ed5cc6c3f3bef2cfb7f798
GTHanson <57633842+GTHanson@users.noreply.github.com>
2023-08-23 22:05:11 -0700

    Update randomnsfw.js

--- interactions/slash/misc/randomnsfw.js
+++ interactions/slash/misc/randomnsfw.js
@@ -5,7 +5,111 @@ const { SlashCommandBuilder } = require("@discordjs/builders");
 const { MessageActionRow, MessageSelectMenu } = require('discord.js');
 const { EmbedBuilder } = require('discord.js');
 
-const nsfw = Nsfw();
+const axios = require('axios');
+
+class Nsfw {
+    constructor() {
+      this.baseUrl =`https://nekobot.xyz/api`
+    }
+  
+    async anal() {
+      const { data } = await axios.get(
+        `${this.baseUrl}/image?type=anal`
+      );
+      const message = await data.message;
+      return message;
+    }
+  
+    async fourk() {
+      const { data } = await axios.get(
+        `${this.baseUrl}/image?type=4k`
+      );
+      const message = await data.message;
+      return message;
+    }
+  
+    async ass() {
+      const { data } = await axios.get(
+        `${this.baseUrl}/image?type=ass`
+      );
+      const message = await data.message;
+      return message;
+    }
+  
+    async gonewild() {
+      const { data } = await axios.get(
+        `${this.baseUrl}/image?type=gonewild`
+      );
+      const message = await data.message;
+      return message;
+    }
+  
+    async pgif() {
+      const { data } = await axios.get(
+        `${this.baseUrl}/image?type=pgif`
+      );
+      const message = await data.message;
+      return message;
+    }
+  
+    async pussy() {
+      const { data }  = await axios.get(
+        `${this.baseUrl}/image?type=pussy`
+      );
+      const message = await data.message;
+       return message;
+  
+    }
+  
+    async thigh() {
+      const { data } = await axios.get(
+        `${this.baseUrl}/image?type=thigh`
+      );
+      const message = await data.message;
+      return message;
+    }
+  
+    async boobs() {
+      const { data } = await axios.get(
+          `${this.baseUrl}/image?type=boobs`
+        );
+        const message = await data.message;
+        return message;
+    }
+  
+    async hentaiass() {
+      const { data } = await axios.get(
+        `${this.baseUrl}/image?type=hass`
+      );
+      const message = await data.message;
+      return message;
+    }
+  
+    async hentai() {
+      const { data } = await axios.get(
+        `${this.baseUrl}/image?type=hentai`
+      );
+      const message = await data.message;
+      return message;
+    }
+  
+    async hmidriff() {
+      const { data } = await axios.get(
+        `${this.baseUrl}/image?type=hmidriff`
+      );
+      const message = await data.message;
+      return message;
+    }
+  
+    async hentaithigh() {
+      const { data } = await axios.get(
+        `${this.baseUrl}/image?type=hthigh`
+      );
+      const message = await data.message;
+      return message;
+    }
+  
+  }
 
 module.exports = {
 	// The only part that makes this different from a default command.
@@ -25,7 +129,7 @@ async function run(interaction, args) {
     var title = "";
     var imageURL = 'https://i.imgur.com/AfFp7pu.png';
 
-    const message = nsfw.ass();
+    const message = await Nsfw.ass();
     console.log(message)
     return;
 
@@ -41,114 +145,4 @@ async function run(interaction, args) {
 	.setTimestamp()
 
 	const replied = await interaction.reply({ embeds: [exampleEmbed], fetchReply: true });
-}
-
-
-const axios = require('axios');
-
-
-class Nsfw {
-  constructor() {
-    this.baseUrl =`https://nekobot.xyz/api`
-  }
-
-  async anal() {
-    const { data } = await axios.get(
-      `${this.baseUrl}/image?type=anal`
-    );
-    const message = await data.message;
-    return message;
-  }
-
-  async fourk() {
-    const { data } = await axios.get(
-      `${this.baseUrl}/image?type=4k`
-    );
-    const message = await data.message;
-    return message;
-  }
-
-  async ass() {
-    const { data } = await axios.get(
-      `${this.baseUrl}/image?type=ass`
-    );
-    const message = await data.message;
-    return message;
-  }
-
-  async gonewild() {
-    const { data } = await axios.get(
-      `${this.baseUrl}/image?type=gonewild`
-    );
-    const message = await data.message;
-    return message;
-  }
-
-  async pgif() {
-    const { data } = await axios.get(
-      `${this.baseUrl}/image?type=pgif`
-    );
-    const message = await data.message;
-    return message;
-  }
-
-  async pussy() {
-    const { data }  = await axios.get(
-      `${this.baseUrl}/image?type=pussy`
-    );
-    const message = await data.message;
-     return message;
-
-  }
-
-  async thigh() {
-    const { data } = await axios.get(
-      `${this.baseUrl}/image?type=thigh`
-    );
-    const message = await data.message;
-    return message;
-  }
-
-  async boobs() {
-    const { data } = await axios.get(
-        `${this.baseUrl}/image?type=boobs`
-      );
-      const message = await data.message;
-      return message;
-  }
-
-  async hentaiass() {
-    const { data } = await axios.get(
-      `${this.baseUrl}/image?type=hass`
-    );
-    const message = await data.message;
-    return message;
-  }
-
-  async hentai() {
-    const { data } = await axios.get(
-      `${this.baseUrl}/image?type=hentai`
-    );
-    const message = await data.message;
-    return message;
-  }
-
-  async hmidriff() {
-    const { data } = await axios.get(
-      `${this.baseUrl}/image?type=hmidriff`
-    );
-    const message = await data.message;
-    return message;
-  }
-
-  async hentaithigh() {
-    const { data } = await axios.get(
-      `${this.baseUrl}/image?type=hthigh`
-    );
-    const message = await data.message;
-    return message;
-  }
-
-  
-
 }
