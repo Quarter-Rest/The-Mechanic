58bc437fec4b062cf3d274540e144aa496b391b5
Ty Morrow <57633827+TMMorrow@users.noreply.github.com>
2024-04-18 12:11:40 -0500

    i love bots

--- interactions/slash/money/coinflip.js
+++ interactions/slash/money/coinflip.js
@@ -1,36 +1,36 @@
-// // Deconstructed the constants we need in this file.
+// Deconstructed the constants we need in this file.
 
-// const { MessageEmbed, Collection } = require("discord.js");
-// const { SlashCommandBuilder } = require("@discordjs/builders");
+const { MessageEmbed, Collection } = require("discord.js");
+const { SlashCommandBuilder } = require("@discordjs/builders");
 
-// module.exports = {
-// 	// The data needed to register slash commands to Discord.
-// 	data: new SlashCommandBuilder()
-// 		.setName("coinflip")
-// 		.setDescription(
-// 			"Create a coinflip challenge."
-// 		).addStringOption(option =>
-// 			option
-// 				.setName('Amount')
-// 				.setDescription('How much to wager. (optional)')
-//                 .setRequired(false)),
+module.exports = {
+	// The data needed to register slash commands to Discord.
+	data: new SlashCommandBuilder()
+		.setName("coinflip")
+		.setDescription(
+			"Create a coinflip challenge."
+		).addStringOption(option =>
+			option
+				.setName('Amount')
+				.setDescription('How much to wager. (optional)')
+                .setRequired(false)),
 
-// 	async execute(interaction) {
-//         await interaction.reply({ content: 'Workin\' on it.', ephemeral: true });
+	async execute(interaction) {
+        await interaction.reply({ content: 'Workin\' on it.', ephemeral: true });
 
-// 		const row = new MessageActionRow()
-// 			.addComponents(
-// 				new MessageButton()
-// 					.setCustomId('heads')
-// 					.setLabel('Heads')
-// 					.setStyle('PRIMARY'),
-// 				new MessageButton()
-// 					.setCustomId('tails')
-// 					.setLabel('Heads')
-// 					.setStyle('PRIMARY')
-// 		);
-//         var wager = interaction.options.getString('Amount');
-//         //var messageText = interaction.options.getString('text');
-// 		interaction.channel.send({ content: row});
-// 	},
-// };
+		const row = new MessageActionRow()
+			.addComponents(
+				new MessageButton()
+					.setCustomId('heads')
+					.setLabel('Heads')
+					.setStyle('PRIMARY'),
+				new MessageButton()
+					.setCustomId('tails')
+					.setLabel('Heads')
+					.setStyle('PRIMARY')
+		);
+        var wager = interaction.options.getString('Amount');
+        //var messageText = interaction.options.getString('text');
+		interaction.channel.send({ content: row});
+	},
+};
--- interactions/slash/money/nickname.js
+++ interactions/slash/money/nickname.js
@@ -1,46 +1,46 @@
-// // Change money nickname.
+// Change money nickname.
 
-// const { MessageEmbed, Collection } = require("discord.js");
-// const { SlashCommandBuilder } = require("@discordjs/builders");
+const { MessageEmbed, Collection } = require("discord.js");
+const { SlashCommandBuilder } = require("@discordjs/builders");
 
-// module.exports = {
-// 	// The data needed to register slash commands to Discord.
-// 	data: new SlashCommandBuilder()
-// 		.setName("nickname")
-// 		.setDescription(
-// 			"Change your nickname to something else when using money commands."
-// 		).addStringOption(option =>
-// 			option
-// 				.setName('Name')
-// 				.setDescription('What to change your nickname to.')
-//                 .setRequired(true)),
+module.exports = {
+	// The data needed to register slash commands to Discord.
+	data: new SlashCommandBuilder()
+		.setName("nickname")
+		.setDescription(
+			"Change your nickname to something else when using money commands."
+		).addStringOption(option =>
+			option
+				.setName('Name')
+				.setDescription('What to change your nickname to.')
+                .setRequired(true)),
 
-// 	async execute(interaction) {
-//         await interaction.reply({ content: 'Workin\' on it.', ephemeral: true });
+	async execute(interaction) {
+        await interaction.reply({ content: 'Workin\' on it.', ephemeral: true });
         
-//         SetNickname(interaction, interaction.options.getString('Name'));
-//         var messageText = interaction.options.getString('Name');
-// 		interaction.channel.send({ content: 'Nickname changed to ' + messageText + '.'});
-// 	},
-// };
+        SetNickname(interaction, interaction.options.getString('Name'));
+        var messageText = interaction.options.getString('Name');
+		interaction.channel.send({ content: 'Nickname changed to ' + messageText + '.'});
+	},
+};
 
-// async function SetNickname(interaction, nick) {
-//     const userId = interaction.member.id;
-//     const query = `UPDATE MONEY SET NICKNAME = ? WHERE ID = ?`;
+async function SetNickname(interaction, nick) {
+    const userId = interaction.member.id;
+    const query = `UPDATE MONEY SET NICKNAME = ? WHERE ID = ?`;
 
-//     return new Promise((resolve, reject) => {
-//         con.query(query, [nick, userId], (err, result) => {
-//             if (err) {
-//                 console.error("Error updating data:", err);
-//                 reject(err);
-//             }
+    return new Promise((resolve, reject) => {
+        con.query(query, [nick, userId], (err, result) => {
+            if (err) {
+                console.error("Error updating data:", err);
+                reject(err);
+            }
       
-//             if (result.affectedRows === 0) {
-//                 console.log("No data found for user:", userId);
-//                 reject("No data found");
-//             }
+            if (result.affectedRows === 0) {
+                console.log("No data found for user:", userId);
+                reject("No data found");
+            }
       
-//             resolve(`Nickname changed to ${nick}`);
-//         });
-//     });
-// }
+            resolve(`Nickname changed to ${nick}`);
+        });
+    });
+}
