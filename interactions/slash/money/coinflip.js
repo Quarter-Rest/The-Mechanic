3f98046fb910a5a26130e2308f9470ab69a95128
Ty Morrow <57633827+TMMorrow@users.noreply.github.com>
2024-04-19 13:59:21 -0500

    Update coinflip.js

--- interactions/slash/money/coinflip.js
+++ interactions/slash/money/coinflip.js
@@ -30,7 +30,7 @@ module.exports = {
         var wager = interaction.options.getString('amount');
 		var response = await interaction.reply({ content: "Please choose heads or tails.", components: [row] });
 
-		const collector = response.createMessageComponentCollector({ componentType: 'BUTTON', time: 3_600_000 });
+		const collector = response.createMessageComponentCollector({ componentType: ComponentType.StringSelect, time: 3_600_000 });
 
 		collector.on('collect', async i => {
 			global.overflowData = interaction;
