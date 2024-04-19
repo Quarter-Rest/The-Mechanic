a792edcd34977fd121a25a07b132ef6398dbd933
Ty Morrow <57633827+TMMorrow@users.noreply.github.com>
2024-04-19 10:02:38 -0500

    trial and error but trying to remove buttons after interaction

--- interactions/buttons/money/heads.js
+++ interactions/buttons/money/heads.js
@@ -1,8 +1,8 @@
 module.exports = {
-	id: "heads",
+    id: "heads",
 
-	async execute(interaction) {
-		var flip = Math.floor(Math.random() * 2);
+    async execute(interaction) {
+        var flip = Math.floor(Math.random() * 2);
         var wOrL = '';
         if(flip == 1) {
             wOrL = '**won**'
@@ -10,9 +10,10 @@ module.exports = {
         else {
             wOrL = '**lost**'
         }
-		await interaction.reply({
-			content: interaction.user.username + " selected heads against " + interaction.member.user.username + ' and ' + wOrL + '!',
-		});
-		return;
-	},
+        await interaction.update({
+            content: interaction.user.username + " selected heads against " + interaction.member.user.username + ' and ' + wOrL + '!',
+            components: [] // This removes the buttons
+        });
+        return;
+    },
 };
--- interactions/buttons/money/tails.js
+++ interactions/buttons/money/tails.js
@@ -1,17 +1,17 @@
 module.exports = {
-    id: "heads",
+    id: "tails",
 
     async execute(interaction) {
         var flip = Math.floor(Math.random() * 2);
         var wOrL = '';
         if(flip == 1) {
-            wOrL = '**won**'
-        }
-        else {
             wOrL = '**lost**'
         }
+        else {
+            wOrL = '**won**'
+        }
         await interaction.update({
-            content: interaction.user.username + " selected heads against " + interaction.member.user.username + ' and ' + wOrL + '!',
+			content: interaction.user.username + " selected tails against " + interaction.member.user.username + ' and ' + wOrL + '!',
             components: [] // This removes the buttons
         });
         return;
