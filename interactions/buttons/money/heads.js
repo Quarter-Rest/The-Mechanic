3172ae139df7adf4dc003539d8007eff170d556e
Ty Morrow <57633827+TMMorrow@users.noreply.github.com>
2024-04-19 15:23:24 -0500

    getting strange

--- interactions/buttons/money/heads.js
+++ interactions/buttons/money/heads.js
@@ -11,12 +11,12 @@ module.exports = {
             wOrL = '**lost**'
         }
 
-        const message = await interaction.channel.messages.fetch(interaction.message.id);
-        const creatorId = global.interactionMap.get(interaction.id); // Retrieve the user's ID
-        global.interactionMap.delete(interaction.id); // Remove the user's ID from the map
+        const message = await interaction.channel.messages.fetch(interaction.message);
+        const creatorId = global.interactionMap.get(interaction.message); // Retrieve the user's ID
+        global.interactionMap.delete(interaction.message); // Remove the user's ID from the map
 
         await interaction.update({
-            content: interaction.user.username + " selected heads against " + creatorId.username + ' and ' + wOrL + '!',
+            content: interaction.user.username + " selected heads against " + creatorId + ' and ' + wOrL + '!',
             components: [] // This removes the buttons
         });
         return;
