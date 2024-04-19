7762b32f296e37cfc1f4bb408a8f4516fb2d69b5
Ty Morrow <morrowtym@gmail.com>
2024-04-18 20:37:00 -0500

    Update coinflip.js

--- interactions/slash/money/coinflip.js
+++ interactions/slash/money/coinflip.js
@@ -1,40 +1,35 @@
 const { MessageEmbed, Collection } = require("discord.js");
 const { SlashCommandBuilder } = require("@discordjs/builders");
 const { MessageActionRow, MessageSelectMenu } = require('discord.js');
+
+// Specifically for button interactions.
 const { MessageButton } = require('discord.js');
 
 module.exports = {
-    data: new SlashCommandBuilder()
-        .setName("coinflip")
-        .setDescription("Create a coinflip challenge.")
-        .addStringOption(option =>
-            option
-                .setName('amount')
-                .setDescription('How much to wager. (optional)')
+	data: new SlashCommandBuilder()
+		.setName("coinflip")
+		.setDescription(
+			"Create a coinflip challenge."
+		).addStringOption(option =>
+			option
+				.setName('amount')
+				.setDescription('How much to wager. (optional)')
                 .setRequired(false)),
 
-    async execute(interaction, args) {
-        const row = new MessageActionRow()
-            .addComponents(
-                new MessageButton()
-                    .setCustomId('heads')
-                    .setLabel('Heads')
-                    .setStyle('PRIMARY'),
-                new MessageButton()
-                    .setCustomId('tails')
-                    .setLabel('Tails')
-                    .setStyle('PRIMARY')
-        );
+	async execute(interaction, args) {
+		const row = new MessageActionRow()
+			.addComponents(
+				new MessageButton()
+					.setCustomId('heads')
+					.setLabel('Heads')
+					.setStyle('PRIMARY'),
+				new MessageButton()
+					.setCustomId('tails')
+					.setLabel('Tails')
+					.setStyle('PRIMARY')
+		);
         var wager = interaction.options.getString('amount');
-        await interaction.reply({ content: "Please choose heads or tails.", components: [row] });
-    },
+        //var messageText = interaction.options.getString('text');
+		await interaction.reply({ content: "Please choose heads or tails.", components: [row] });
+	},
 };
-
-// Listen for interaction create event
-client.on('interactionCreate', async interaction => {
-    if (!interaction.isButton()) return;
-    if (interaction.customId === 'heads' || interaction.customId === 'tails') {
-        // Delete the message after the button is clicked
-        await interaction.message.delete();
-    }
-});
