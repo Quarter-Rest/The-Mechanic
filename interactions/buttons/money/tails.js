d69f54044158007af6482b645aa60dbead03698d
Ty Morrow <57633827+TMMorrow@users.noreply.github.com>
2024-04-19 10:09:17 -0500

    trying something different, moving modules to larger file

--- interactions/buttons/money/tails.js
+++ interactions/buttons/money/tails.js
@@ -1,19 +1,19 @@
-module.exports = {
-    id: "tails",
+// module.exports = {
+//     id: "tails",
 
-    async execute(interaction) {
-        var flip = Math.floor(Math.random() * 2);
-        var wOrL = '';
-        if(flip == 1) {
-            wOrL = '**lost**'
-        }
-        else {
-            wOrL = '**won**'
-        }
-        await interaction.update({
-			content: interaction.user.username + " selected tails against " + interaction.member.user.username + ' and ' + wOrL + '!',
-            components: [] // This removes the buttons
-        });
-        return;
-    },
-};
+//     async execute(interaction) {
+//         var flip = Math.floor(Math.random() * 2);
+//         var wOrL = '';
+//         if(flip == 1) {
+//             wOrL = '**lost**'
+//         }
+//         else {
+//             wOrL = '**won**'
+//         }
+//         await interaction.update({
+// 			content: interaction.user.username + " selected tails against " + interaction.member.user.username + ' and ' + wOrL + '!',
+//             components: [] // This removes the buttons
+//         });
+//         return;
+//     },
+// };
--- interactions/slash/money/coinflip.js
+++ interactions/slash/money/coinflip.js
@@ -1,9 +1,9 @@
 const { MessageEmbed, Collection } = require("discord.js");
 const { SlashCommandBuilder } = require("@discordjs/builders");
 const { MessageActionRow, MessageSelectMenu } = require('discord.js');
-
-// Specifically for button interactions.
 const { MessageButton } = require('discord.js');
+
+const usernames = new Map();
 
 module.exports = {
 	data: new SlashCommandBuilder()
@@ -29,7 +29,53 @@ module.exports = {
 					.setStyle('PRIMARY')
 		);
         var wager = interaction.options.getString('amount');
-        //var messageText = interaction.options.getString('text');
 		await interaction.reply({ content: "Please choose heads or tails.", components: [row] });
+
+		// Store the username of the person who sent the coinflip command.
+        usernames.set(interaction.message.id, interaction.user.username);
 	},
+};
+
+module.exports = {
+    id: "heads",
+
+    async execute(interaction) {
+        var flip = Math.floor(Math.random() * 2);
+        var wOrL = '';
+        if(flip == 1) {
+            wOrL = '**won**'
+        }
+        else {
+            wOrL = '**lost**'
+        }
+        // Retrieve the username of the person who sent the coinflip command.
+        const username = usernames.get(interaction.message.id);
+        await interaction.update({
+            content: interaction.user.username + " selected heads against " + username + ' and ' + wOrL + '!',
+            components: [] // This removes the buttons
+        });
+        return;
+    },
+};
+
+module.exports = {
+    id: "tails",
+
+    async execute(interaction) {
+        var flip = Math.floor(Math.random() * 2);
+        var wOrL = '';
+        if(flip == 1) {
+            wOrL = '**lost**'
+        }
+        else {
+            wOrL = '**won**'
+        }
+        // Retrieve the username of the person who sent the coinflip command.
+        const username = usernames.get(interaction.message.id);
+        await interaction.update({
+            content: interaction.user.username + " selected tails against " + username + ' and ' + wOrL + '!',
+            components: [] // This removes the buttons
+        });
+        return;
+    },
 };
