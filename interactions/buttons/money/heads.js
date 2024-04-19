b34b15172d8ad9621bd0dc62c08020c24fa85fb0
Ty Morrow <57633827+TMMorrow@users.noreply.github.com>
2024-04-19 10:38:59 -0500

    revert and trying to fix this coin

--- interactions/buttons/money/heads.js
+++ interactions/buttons/money/heads.js
@@ -1,19 +1,20 @@
-// module.exports = {
-//     id: "heads",
+module.exports = {
+    id: "heads",
 
-//     async execute(interaction) {
-//         var flip = Math.floor(Math.random() * 2);
-//         var wOrL = '';
-//         if(flip == 1) {
-//             wOrL = '**won**'
-//         }
-//         else {
-//             wOrL = '**lost**'
-//         }
-//         await interaction.update({
-//             content: interaction.user.username + " selected heads against " + interaction.member.user.username + ' and ' + wOrL + '!',
-//             components: [] // This removes the buttons
-//         });
-//         return;
-//     },
-// };
+    async execute(interaction) {
+        var flip = Math.floor(Math.random() * 2);
+        var wOrL = '';
+        if(flip == 1) {
+            wOrL = '**won**'
+        }
+        else {
+            wOrL = '**lost**'
+        }
+        console.log(interaction.getOriginalReply.message.user.username);
+        await interaction.update({
+            content: interaction.user.username + " selected heads against " + interaction.member.user.username + ' and ' + wOrL + '!',
+            components: [] // This removes the buttons
+        });
+        return;
+    },
+};
