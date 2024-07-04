b0fc184f217a99e13fc7c2874de2dcbba14bafff
GTHanson <57633842+GTHanson@users.noreply.github.com>
2024-07-04 17:32:35 -0500

    no message

---
+++ interactions/slash/misc/picklelog.js
@@ -0,0 +1,39 @@
+
+const { MessageEmbed, Collection } = require("discord.js");
+const { SlashCommandBuilder } = require("@discordjs/builders");
+const { MessageActionRow, MessageSelectMenu } = require('discord.js');
+const PicklerRoleID = "1257548633956421722";
+
+module.exports = {
+	// The data needed to register slash commands to Discord.
+	data: new SlashCommandBuilder()
+		.setName("picklelog")
+		.setDescription(
+			"Log a game of Pickleball."
+		),
+
+	async execute(interaction) {
+        // Get all members in the guild
+        const members = interaction.guild.members.cache;
+        // Filter out anyone without the Pickler role
+        const players = members.filter(member => member.roles.cache.has(PicklerRoleID));
+        
+        let playerOptions = []
+        players.forEach(member => {
+            playerOptions.push( {
+                label: member.displayName + " (" + member.user.username + ")",
+                description: 'Maybe their career stats here?',
+                value: member,
+            });
+        });
+
+        const row = new MessageActionRow().addComponents(
+            new MessageSelectMenu()
+                .setCustomId('select')
+                .setPlaceholder('Select a player.')
+                .addOptions(playerOptions),
+        );
+
+		await interaction.reply({ content: 'test!', components: [row] });
+	},
+};
