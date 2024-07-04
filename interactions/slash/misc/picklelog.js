927eeafadd9efed99b057cd767da08d4e721ddba
GTHanson <57633842+GTHanson@users.noreply.github.com>
2024-07-04 17:39:14 -0500

    no message

--- interactions/slash/misc/picklelog.js
+++ interactions/slash/misc/picklelog.js
@@ -34,6 +34,20 @@ module.exports = {
                 .addOptions(playerOptions)
         );
 
-		await interaction.reply({ content: 'test!', components: [row] });
+        const userSelect = new UserSelectMenuBuilder()
+			.setCustomId('users')
+			.setPlaceholder('Select multiple users.')
+			.setMinValues(1)
+			.setMaxValues(10);
+
+		const row1 = new ActionRowBuilder()
+			.addComponents(userSelect);
+
+		await interaction.reply({
+			content: 'Select users:',
+			components: [row1],
+		});
+
+		//await interaction.reply({ content: 'test!', components: [row] });
 	},
 };
