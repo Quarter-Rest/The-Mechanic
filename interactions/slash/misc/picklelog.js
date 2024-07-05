9f4f185481e7edae7dbee9eae1ea6468d4c75057
Griffon Hanson <57633842+GTHanson@users.noreply.github.com>
2024-07-05 18:20:04 -0500

    no message

--- interactions/slash/misc/picklelog.js
+++ interactions/slash/misc/picklelog.js
@@ -1,6 +1,6 @@
 
-const { ActionRowBuilder, StringSelectMenuBuilder, StringSelectMenuOptionBuilder, SlashCommandBuilder } = require('discord.js');
-const PicklerRoleID = "1257548633956421722";
+const { ActionRowBuilder, StringSelectMenuBuilder, StringSelectMenuOptionBuilder, SlashCommandBuilder } = require('discord.js')
+const PicklerRoleID = "1257548633956421722"
 
 module.exports = {
 	// The data needed to register slash commands to Discord.
@@ -26,54 +26,52 @@ module.exports = {
 
 	async execute(interaction) {
         // Immediately send a reply
-        await interaction.reply({ content: "Loading...", ephemeral: true });
+        await interaction.reply({ content: "Loading...", ephemeral: true })
 
-        const numPlayersWin = interaction.options.getInteger("winningteamcount");
-        const numPlayersLose = interaction.options.getInteger("losingteamcount");
+        const numPlayersWin = interaction.options.getInteger("winningteamcount")
+        const numPlayersLose = interaction.options.getInteger("losingteamcount")
 
         // Get all members in the guild
-        const members = await interaction.guild.members.fetch();
+        const members = await interaction.guild.members.fetch()
         // Filter out anyone without the Pickler role
-        const players = members.filter(member => member.roles.cache.has(PicklerRoleID));
+        const players = members.filter(member => member.roles.cache.has(PicklerRoleID))
 
         let playerOptions = []
         players.forEach(member => {
             let option = new StringSelectMenuOptionBuilder()
                 .setLabel(member.displayName + " (" + member.user.username + ")")
                 .setDescription("Maybe their career stats here?")
-                .setValue(member.id);
+                .setValue(member.id)
 
-            playerOptions.push( option );
-        });
+            playerOptions.push( option )
+        })
 
         // Add the option for custom players
         let customPlayerOption = new StringSelectMenuOptionBuilder()
                 .setLabel("Custom Player")
                 .setDescription("Will not track stats for this player.")
-                .setValue( "custom");
-        playerOptions.push( customPlayerOption );
+                .setValue( "custom")
+        playerOptions.push( customPlayerOption )
 
         // Setup winner response
         let winningRows = []
         for (let i = 0; i < numPlayersWin; i++) {
-            const row = new ActionRowBuilder().addComponents(
-                new StringSelectMenuBuilder()
-                    .setCustomId("select" + i)
-                    .setPlaceholder("Select a player.")
-                    .addOptions(playerOptions)
-            ); 
-            winningRows.push(row)
+            winningRows.push(MakePlayerSelectionRow(playerOptions, i))
         }
         
-		const winnerReply = await interaction.editReply({ content: 'Select winners.', components: winningRows });
+		const winnerReply = await interaction.editReply({ content: 'Select winners.', components: winningRows })
         
-        var winners
-        try {
-            winners = await winnerReply.awaitMessageComponent({ time: 60_000 });
-        } catch (e) {
-            await interaction.editReply({ content: 'Confirmation not received within 1 minute, cancelling', components: [] });
-            return
+        var winners = []
+        for (let i = 0; i < numPlayersWin; i++) {
+            try {
+                const reply = await winnerReply.awaitMessageComponent({ time: 60_000 })
+                winners.push(reply)
+            } catch (e) {
+                await interaction.editReply({ content: 'Confirmation not received within 1 minute, cancelling', components: [] })
+                return
+            }
         }
+        
 
         // Setup loser response
         let losingRows = []
@@ -81,26 +79,28 @@ module.exports = {
             losingRows.push(MakePlayerSelectionRow(playerOptions, i))
         }
 
-        const loserReply = await interaction.editReply({ content: 'Select losers.', components: losingRows });
+        const loserReply = await interaction.editReply({ content: 'Select losers.', components: losingRows })
         
         var losers
         try {
-            losers = await loserReply.awaitMessageComponent({ time: 60_000 });
+            losers = await loserReply.awaitMessageComponent({ time: 60_000 })
         } catch (e) {
-            await interaction.editReply({ content: 'Confirmation not received within 1 minute, cancelling', components: [] });
+            await interaction.editReply({ content: 'Confirmation not received within 1 minute, cancelling', components: [] })
             return
         }
 
 
 	},
-};
+}
 
 
 async function MakePlayerSelectionRow(playerOptions, uid) {
-    return new ActionRowBuilder().addComponents(
+    const row = new ActionRowBuilder().addComponents(
         new StringSelectMenuBuilder()
             .setCustomId("select" + uid)
             .setPlaceholder("Select a player.")
             .addOptions(playerOptions)
-    ); 
+    ) 
+    
+    return row
 }
