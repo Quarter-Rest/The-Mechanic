e9c7d92cd21d1e9f0fadacbf6693cadf2d16b2f2
Griffon Hanson <57633842+GTHanson@users.noreply.github.com>
2024-07-06 16:29:27 -0500

    no message

--- events/selectInteraction.js
+++ events/selectInteraction.js
@@ -15,38 +15,38 @@ module.exports = {
 
 	async execute(interaction) {
 		// // Deconstructed client from interaction object.
-		// const { client } = interaction;
+		const { client } = interaction;
 
-		// // Checks if the interaction is a select menu interaction (to prevent weird bugs)
+		// Checks if the interaction is a select menu interaction (to prevent weird bugs)
 
-		// if (!interaction.isStringSelectMenu()) return;
-		// /**
-		//  * @description The Interaction command object
-		//  * @type {import("discord.js").SelectMenuInteraction}
-		//  */
+		if (!interaction.isStringSelectMenu()) return;
+		/**
+		 * @description The Interaction command object
+		 * @type {import("discord.js").SelectMenuInteraction}
+		 */
 
-		// const command = client.selectCommands.get(interaction.customId);
+		const command = client.selectCommands.get(interaction.customId);
 
-		// // If the interaction is not a command in cache, return error message.
-		// // You can modify the error message at ./messages/defaultSelectError.js file!
+		// If the interaction is not a command in cache, return error message.
+		// You can modify the error message at ./messages/defaultSelectError.js file!
 
-		// // if (!command) {
-		// // 	await require("../messages/defaultSelectError").execute(interaction);
-		// // 	return;
-		// // }
+		if (!command) {
+			await require("../messages/defaultSelectError").execute(interaction);
+			return;
+		}
 
-		// // A try to execute the interaction.
+		// A try to execute the interaction.
 
-		// try {
-		// 	await command.execute(interaction);
-		// 	return;
-		// } catch (err) {
-		// 	console.error(err);
-		// 	await interaction.reply({
-		// 		content: "There was an issue while executing that select menu option!",
-		// 		ephemeral: true,
-		// 	});
-		// 	return;
-		// }
+		try {
+			await command.execute(interaction);
+			return;
+		} catch (err) {
+			console.error(err);
+			await interaction.reply({
+				content: "There was an issue while executing that select menu option!",
+				ephemeral: true,
+			});
+			return;
+		}
 	},
 };
