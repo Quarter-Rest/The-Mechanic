2cbee3dcdea896f3b7eddb47ea9b10ef87dd4126
Griffon Hanson <57633842+GTHanson@users.noreply.github.com>
2024-07-06 17:38:11 -0500

    no message

--- interactions/slash/misc/picklelog.js
+++ interactions/slash/misc/picklelog.js
@@ -1,7 +1,7 @@
 
 const { ActionRowBuilder, StringSelectMenuBuilder, StringSelectMenuOptionBuilder, SlashCommandBuilder, ComponentType } = require('discord.js')
 const PicklerRoleID = "1257548633956421722"
-const selectMenuID = "select"
+
 module.exports = {
 	// The data needed to register slash commands to Discord.
 	data: new SlashCommandBuilder()
@@ -54,14 +54,14 @@ module.exports = {
         playerOptions.push( customPlayerOption )
 
         // Setup winner response
-        let winningRow = MakePlayerSelection(playerOptions, numPlayersWin)
+        let winningRow = MakePlayerSelection(playerOptions, numPlayersWin, "selectWinners")
         
 		let newReply = await interaction.editReply({ content: 'Select winners.', components: [winningRow] })
         
         const collector = newReply.createMessageComponentCollector({ componentType: ComponentType.StringSelect, time: 3_600_000 });
 
         collector.on('collect', async i => {
-            if (OnCollect(i, numPlayersWin, true))
+            if (OnCollect(i, numPlayersWin, true, "selectWinners"))
             {
                 collector.stop()
                 i.deferUpdate()
@@ -70,11 +70,12 @@ module.exports = {
 
         collector.on('end', async (interact) => {
             // Setup loser response
-            let losingRow = MakePlayerSelection(playerOptions, numPlayersLose)
-            await interaction.editReply({ content: 'Select losers.', components: [losingRow] })
+            let losingRow = MakePlayerSelection(playerOptions, numPlayersLose, "selectLosers")
+            newReply = await interaction.editReply({ content: 'Select losers.', components: [losingRow] })
+            const loserCollecter = newReply.createMessageComponentCollector({ componentType: ComponentType.StringSelect, time: 3_600_000 });
 
-            collector.on('collect', async i => {
-                if (OnCollect(i, numPlayersLose, false))
+            loserCollecter.on('collect', async i => {
+                if (OnCollect(i, numPlayersLose, false, "selectLosers"))
                 {
                     collector.stop()
                     i.deferUpdate()
@@ -87,7 +88,7 @@ module.exports = {
 	},
 }
 
-function OnCollect(interaction, expectedSize, isWinners)
+function OnCollect(interaction, expectedSize, isWinners, selectMenuID)
 {
     console.log(interaction.customId + " || " + interaction.values.length + " | " + expectedSize)
     if (interaction.customId != selectMenuID || interaction.values.length != expectedSize)
@@ -97,7 +98,7 @@ function OnCollect(interaction, expectedSize, isWinners)
     return true
 }
 
-function MakePlayerSelection(playerOptions, numPlayers) {
+function MakePlayerSelection(playerOptions, numPlayers, selectMenuID) {
     const row = new ActionRowBuilder().addComponents(
         new StringSelectMenuBuilder()
             .setCustomId(selectMenuID)
