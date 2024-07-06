cd2455f625e6b1fdab25bacff18ac592d69f540a
Griffon Hanson <57633842+GTHanson@users.noreply.github.com>
2024-07-06 16:23:16 -0500

    no message

--- events/contextInteraction.js
+++ events/contextInteraction.js
@@ -19,7 +19,7 @@ module.exports = {
 
 		// Checks if the interaction is a context interaction (to prevent weird bugs)
 
-		//if (!interaction.isContextMenu()) return;
+		if (!interaction.isUserContextMenuCommand()) return;
 
 		/**********************************************************************/
 
