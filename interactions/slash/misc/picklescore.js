1a75a02c82a3fcc81b0ebc3ac80adbea3ddf456b
Griffon <gtkhanson@gmail.com>
2024-07-10 15:40:41 -0500

    no message

--- interactions/slash/misc/picklescore.js
+++ interactions/slash/misc/picklescore.js
@@ -29,8 +29,8 @@ module.exports = {
 		const members = await interaction.guild.members.fetch()
         // Filter out anyone without the Pickler role
         const picklers = members.filter(member => member.roles.cache.has(global.PicklerRoleID))
-		console.log(JSON.stringify(picklers))
-        let playerScores = []
+
+		let playerScores = []
 		for (let i = 0; i < picklers.length; i++) {
 			const member = picklers[i]
 			const id = member.userId
