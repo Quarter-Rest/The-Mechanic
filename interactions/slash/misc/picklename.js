9748ba9fa3fe1c8341f2bf651145b7751656d3fd
Griffon <gtkhanson@gmail.com>
2024-07-10 16:44:47 -0500

    no message

---
+++ interactions/slash/misc/picklename.js
@@ -0,0 +1,33 @@
+
+const {  SlashCommandBuilder, EmbedBuilder } = require('discord.js')
+
+const {InteractionAPI} = require('../interaction-api')
+
+const sqlTableName = "PICKLEBALL"
+
+module.exports = {
+	// The data needed to register slash commands to Discord.
+	data: new SlashCommandBuilder()
+		.setName("picklename")
+		.setDescription(
+			"Rename yourself on the Pickleball scoreboard."
+		)
+		.addStringOption((option) =>
+			option
+				.setName("newname")
+				.setDescription("Your new name.")
+				.setRequired(true)
+		),
+
+	async execute(interaction) {
+        // Immediately send a reply
+        const originalReply = await interaction.reply({ content: "Loading..."})
+		await InteractionAPI.CheckUserInTable(interaction.user.id, sqlTableName)
+
+		const name = interaction.options.getString("newname")
+
+		await InteractionAPI.SetValueInTable(interaction.user.id, sqlTableName, "NICKNAME", name)
+
+		await interaction.editReply({ content: "Done!", content: [] })
+	},
+}
